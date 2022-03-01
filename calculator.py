af50e1414df5cbeb4a92176b96666b12ffe9311d
aleksEvstigneev <100333161+aleksEvstigneev@users.noreply.github.com>
2022-02-28 23:18:12 -0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+#calculator
+
+grossIncome = float(input('Please, Enter your Gross Income: '))
+
+dependents = int(input('Please, enter number of dependetnts: '))
+
+incomeTax = float(((grossIncome - 10000) - dependents * 3000) * 0.2)
+incomeTax = '{:.2f}'.format(incomeTax)
+incomeTaxStr = str(incomeTax)
+
+print('Enter the gross income:', '{:.2f}'.format(grossIncome))
+print('Enter the number of dependents:', dependents)
+print('The income tax is $' + incomeTaxStr)
+
+input('\n\n Press enter key to exit.')
